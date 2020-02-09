fc3ef9ea2158b0c20802a41e1db39adda0fc91c3
cangir <ahmetcangir07@gmail.com>
2020-02-09 16:59:51 +0100

    feat: create site buildtime hook

--- src/hooks/use-site-buildtime.tsx
+++ src/hooks/use-site-buildtime.tsx
@@ -1,25 +1,21 @@
 import { graphql, useStaticQuery } from "gatsby";
 
 type Props = {
-  site: {
-    buildTime: string;
-  };
+	site: {
+		buildTime: string;
+	};
 };
 
-{
-  /* TODO: fix warning Missing return type on function */
-}
-
 export const useSiteBuildTime = () => {
-  const data = useStaticQuery<Props>(graphql`
-    query {
-      site {
-        buildTime(formatString: "YYYY-MM-DD")
-      }
-    }
-  `);
+	const data = useStaticQuery<Props>(graphql`
+		query {
+			site {
+				buildTime(formatString: "YYYY-MM-DD")
+			}
+		}
+	`);
 
-  return data.site.buildTime;
+	return data.site.buildTime;
 };
 
 export default useSiteBuildTime;
