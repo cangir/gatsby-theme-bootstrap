341e6e5d7581119173cad3bf8f845fcec6edb2d9
cangir <ahmetcangir07@gmail.com>
2020-02-09 16:52:38 +0100

    create website config

--- src/website-config.ts
+++ src/website-config.ts
@@ -1,14 +1,14 @@
 "use strict";
 
 module.exports = {
-  siteTitle: "Gatsby Bootstrap Theme",
-  siteTitleAlt: "Gatsby Bootstrap Theme Site Alternative Title",
-  siteHeadline: "Gatsby Bootstrap Theme Site Headline",
-  siteUrl: "https://gatsby-theme-bootstrap.netlify.com",
-  siteDescription: "Gatsby Bootstrap Theme Site Description",
-  siteLanguage: "en",
-  siteImage: "banner.png",
-  author: "cangir",
-  basePath: "/",
-  postsPerPage: 4,
+	siteTitle: "Gatsby Bootstrap Theme",
+	siteTitleAlt: "Gatsby Bootstrap Theme Site Alternative Title",
+	siteHeadline: "Gatsby Bootstrap Theme Site Headline",
+	siteUrl: "https://gatsby-theme-bootstrap.netlify.com",
+	siteDescription: "Gatsby Bootstrap Theme Site Description",
+	siteLanguage: "en",
+	siteImage: "banner.png",
+	author: "cangir",
+	basePath: "/",
+	postsPerPage: 4,
 };
