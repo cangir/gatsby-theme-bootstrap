99334097646f7780be073bd935e5d08bd2823e49
cangir <ahmetcangir07@gmail.com>
2020-02-09 23:57:50 +0100

    feat: add divider

--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import clsx from 'clsx';
 
-export const Divider: React.FC = () => {
+interface Props {
+	position?: string;
+	variant?: string;
+}
+
+export const Divider = ({position, variant}: Props) => {
 	return (
-		<section className="promo bg-facebook caxton-shape-divider">
-			<div className="">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="xMidYMax slice">
-					<path d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
-				</svg>
-			</div>
+		<section className={
+			clsx(
+				"divider", 
+				position && position == "top" ? "top-0" : "bottom-0", 
+				variant && "divider-" + variant
+				)}>
+			<svg
+      width={2560}
+      height={100}
+      preserveAspectRatio="none"
+			viewBox="0 0 2560 100"
+			className={position}
+    >
+			<path d="M2560 0v100H0z" fill="#fff" />
+    </svg>
 		</section>
 	);
 };
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { Container, Breadcrumb } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 // import { Parallax } from "react-parallax";
+import { Breadcrumb, Divider } from "..";
 
 export const Hero: React.FC = () => {
 	return (
 		<section
-			className="promo promo-block overlay overlay--dark gradient-primary"
-			style={{ background: "url(http://localhost:8000/img/img5.jpg)" }}
+			className="promo"
+			style={{ background: "center url(./img/bg.jpg)" }}
 		>
 			<Container className="d-flex promo-container justify-content-start position-relative">
 				<div className="align-self-end p-5">
 					<h1>Can I upgrade my plan at any time?</h1>
-					<Breadcrumb className="breadcrumb-arrows breadcrumb-alternate">
-						<Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-						<Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">Library</Breadcrumb.Item>
-						<Breadcrumb.Item active>Data</Breadcrumb.Item>
-					</Breadcrumb>
+					<Breadcrumb />
 				</div>
 			</Container>
+			<Divider />
 		</section>
 	);
 };
