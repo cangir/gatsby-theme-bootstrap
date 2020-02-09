b388b2bbbc958ab3e18eb405ee8e2873425589c9
cangir <ahmetcangir07@gmail.com>
2020-02-09 16:38:01 +0100

    feat: create use all posts hook

--- src/hooks/use-all-posts.tsx
+++ src/hooks/use-all-posts.tsx
@@ -1,48 +1,39 @@
 import { graphql, useStaticQuery } from "gatsby";
 
 type Props = {
-  allMdx: {
-    nodes: {
-      id: string;
-      body: string;
-      excerpt: string;
-      frontmatter: {
-        title: string;
-        date: string;
-        tags: string[];
-      };
-      fields: {
-        slug: string;
-      };
-    }[];
-  };
+	allMdx: {
+		nodes: {
+			id: string;
+			body: string;
+			excerpt: string;
+			frontmatter: {
+				title: string;
+				date: string;
+				slug: string;
+				tags: string[];
+			};
+		}[];
+	};
 };
 
-{
-  /* TODO: fix warning Missing return type on function */
-}
-
 export const useAllPosts = () => {
-  const data = useStaticQuery<Props>(graphql`
-    query {
-      allMdx(sort: { order: ASC, fields: [frontmatter___date] }) {
-        nodes {
-          id
-          excerpt(pruneLength: 100)
-          frontmatter {
-            title
-            date(formatString: "YYYY-MM-DD")
-            slug
-            tags
-          }
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  `);
-  return data.allMdx.nodes;
+	const data = useStaticQuery<Props>(graphql`
+		query {
+			allMdx(sort: { order: ASC, fields: [frontmatter___date] }) {
+				nodes {
+					id
+					excerpt(pruneLength: 100)
+					frontmatter {
+						title
+						date(formatString: "YYYY-MM-DD")
+						slug
+						tags
+					}
+				}
+			}
+		}
+	`);
+	return data.allMdx.nodes;
 };
 
 export default useAllPosts;
