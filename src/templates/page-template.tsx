379354baac12406994f7f4ecad4b982b5c964ade
cangir <ahmetcangir07@gmail.com>
2020-02-11 00:55:17 +0100

    refactor: initial page template

--- src/templates/page-template.tsx
+++ src/templates/page-template.tsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from "../components/layout/layout";
+import { Post } from "../models";
 
-const PageTemplate: React.FCX<{}> = () => {
-	return <Layout>Page Template</Layout>;
+type Props = {
+	data: {
+		mdx: Post;
+	};
 };
+
+const PageTemplate = ({ data }: Props) => {
+	const post = data.mdx;
+	return (
+		<Layout title={post.frontmatter.title}>
+			<MDXRenderer>{post.body}</MDXRenderer>
+		</Layout>
+	);
+};
+
+export const query = graphql`
+	query PageBySlug($slug: String!) {
+		mdx(frontmatter: { slug: { eq: $slug } }) {
+			id
+			body
+			frontmatter {
+				tags
+				slug
+				category
+				date
+				draft
+				description
+				modified
+				template
+				title
+			}
+		}
+	}
+`;
 
 export default PageTemplate;
