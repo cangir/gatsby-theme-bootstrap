d45dde6381503d4584229a7b5614615ceb73976a
cangir <ahmetcangir07@gmail.com>
2020-02-11 00:51:32 +0100

    refactor: post template

--- src/templates/post-template.tsx
+++ src/templates/post-template.tsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from "../components/layout/layout";
+import { Post } from "../models";
 
-const PostTemplate: React.FCX<{}> = () => {
-	return <Layout>Post Template</Layout>;
+type Props = {
+	data: {
+		mdx: Post;
+	};
 };
+
+const PostTemplate = ({ data }: Props) => {
+	const post = data.mdx;
+	return (
+		<Layout title={post.frontmatter.title} breadcrumb>
+			<MDXRenderer>{post.body}</MDXRenderer>
+		</Layout>
+	);
+};
+
+export const query = graphql`
+	query PostBySlug($slug: String!) {
+		mdx(frontmatter: { slug: { eq: $slug } }) {
+			id
+			body
+			frontmatter {
+				tags
+				slug
+				category
+				date
+				draft
+				description
+				modified
+				template
+				title
+			}
+		}
+	}
+`;
 
 export default PostTemplate;
