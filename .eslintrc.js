762329eea4886df2142f88c31bc6ced84c02ce61
cangir <ahmetcangir07@gmail.com>
2020-02-12 23:09:30 +0100

    add hooks rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     node: true,
     es6: true
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -27,7 +27,9 @@ module.exports = {
   },
   rules: {
     'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of hooks
+    'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
   },
   overrides: [
     // Override some TypeScript rules just for .js files
